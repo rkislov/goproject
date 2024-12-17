fa80c0498bdcac8088943a116f0c5d5c0d7d1643
Roman Kislov <roman@kislovs.ru>
2024-12-17 08:01:05 +0300

    first handler

--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("hello")
-}
-
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("/hello", hello)
+	NewHelloHandler(router)
+
 	server := http.Server{
 		Addr:    ":8081",
 		Handler: router,
