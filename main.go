2f426f35b74f56f1971ab00eedfca65d0cb2cc60
Roman Kislov <roman@kislovs.ru>
2024-12-17 07:36:42 +0300

    own htt server

--- main.go
+++ main.go
@@ -10,7 +10,13 @@ func hello(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/hello", hello)
+	router := http.NewServeMux()
+	router.HandleFunc("/hello", hello)
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
 	fmt.Println("Server listen on port 8081")
-	http.ListenAndServe(":8081", nil)
+	server.ListenAndServe()
 }
