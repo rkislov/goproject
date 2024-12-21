2fe0e1320bcf09576add0e958db9e2fcebb10198
Roman Kislov <roman@kislovs.ru>
2024-12-21 11:15:09 +0300

    config

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"go-adv-demo/internal/hello"
 	"net/http"
+
+	"go-adv-demo/config"
 )
 
 func main() {
+	conf := config.LoadConfig()
 	router := http.NewServeMux()
 	hello.NewHelloHandler(router)
 
